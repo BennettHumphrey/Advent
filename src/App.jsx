20d694de6d4cc530d9d9db75dcfbd8bdf96049c6
Bennett Humphrey <bennetthumphrey98@gmail.com>
2023-03-17 14:49:04 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Test from './components/test'
+
+function App() {
+
+  return (
+    <div >
+      <Test  />
+      <p className='text-red-500' >testRed</p>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      xsm: '480px',
+      sm: '550px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        "text-dark": "#666666",
+        "line": "#BBBBBB",
+        "text-footer": "#16161D",
+        "footer": "#BDBDBD"
+      },
+    },
+  },
+  plugins: [],
+}
